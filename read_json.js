633b54bb0b9a841f975da792fa2acadbb179671e
Enderman-07 <104712238+Enderman-07@users.noreply.github.com>
2024-06-08 14:12:55 +0800

    Update read_json.js

--- read_json.js
+++ read_json.js
@@ -14,7 +14,7 @@ function loadJSON(callback) {
 function displayData(data) {
     var jsonData = JSON.parse(data);
     var container1 = document.getElementById('parameter1');
-    var phone = getcookie();
+    var phone = getvalue();
     for(let i=0;i<jsonData.length;++i){
         if(jsonData[i]["机型"]==phone){
             container1.innerHTML = '内存:' + jsonData[i]['内存'] + '<br>CPU型号:' + jsonData[i]['CPU型号'];
@@ -47,32 +47,15 @@ function change_opt(data) {
 }
 
 
-function setCookie(name, value, days) {  
-    var expires = "";  
-    if (days) {  
-        var date = new Date();  
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));  
-        expires = "; expires=" + date.toUTCString();  
-    }  
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";  
+function setvalue() {  
+    var phone = document.getElementById("phones").value;
+    localStorage.setItem('phone',phone);  
 }  
 
-function getCookie(name) {  
-    var nameEQ = name + "=";  
-    var ca = document.cookie.split(';');  
-    for (var i = 0; i < ca.length; i++) {  
-        var c = ca[i];  
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);  
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);  
+function getvalue() {  
+    var value = localStorage.getItem('phone');  
+    if (value) {  
+        return value;
     }  
-    return null;  
-}  
-function setcookie() {  
-    var name = document.getElementById('phones').value;
-    setCookie("phone_name", name, 1)// 设置cookie名为phone_name，有效期为1天  
-}  
-
-function getcookie() {  
-    var name = getCookie("phone_name");
-    return name
+    return null;
 } 
