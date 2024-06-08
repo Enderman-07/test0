cdd17574df4b16eb1fd556d5a2c0ae7868a703a5
Enderman-07 <104712238+Enderman-07@users.noreply.github.com>
2024-06-08 11:47:53 +0800

    Add files via upload

--- read_json.js
+++ read_json.js
@@ -12,17 +12,18 @@ function loadJSON(callback) {
 
 // 在页面上显示JSON数据
 function displayData(data) {
+    console.log("1");
     var jsonData = JSON.parse(data);
     var container1 = document.getElementById('parameter1');
-    var phone = getCookie();
+    var phone = getcookie();
     for(let i=0;i<jsonData.length();++i){
         if(json[i]["机型"]==phone){
             container1.innerHTML = '内存:' + jsonData[i]['内存'] + '<br>CPU型号:' + jsonData[i]['CPU型号'];
             break;
         } 
     }
-    var container2 = document.getElementById('parameter2');
-    var phone2 = document.getElementById('phones').value;
+    var container2 = document.getElementById("parameter2");
+    var phone2 = document.getElementById("phones").value;
     for(let i=0;i<jsonData.length();++i){
         if(json[i]["机型"]==phone2){
             container2.innerHTML = '内存:' + jsonData[i]['内存'] + '<br>CPU型号:' + jsonData[i]['CPU型号'];
@@ -47,14 +48,32 @@ function change_opt(data) {
 }
 
 
-
-
-function setCookie() {  
-    var name = document.getElementById('phones').value;
-    document.cookie = name
+function setCookie(name, value, days) {  
+    var expires = "";  
+    if (days) {  
+        var date = new Date();  
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));  
+        expires = "; expires=" + date.toUTCString();  
+    }  
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";  
 }  
 
-function getCookie() {  
-    var name = document.cookie
+function getCookie(name) {  
+    var nameEQ = name + "=";  
+    var ca = document.cookie.split(';');  
+    for (var i = 0; i < ca.length; i++) {  
+        var c = ca[i];  
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);  
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);  
+    }  
+    return null;  
+}  
+function setcookie() {  
+    var name = document.getElementById('phones').value;
+    setCookie("phone_name", name, 1)// 设置cookie名为phone_name，有效期为1天  
+}  
+
+function getcookie() {  
+    var name = getCookie("phone_name");
     return name
 } 
