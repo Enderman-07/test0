c5bc50db9d154f5ab1c9786a43e258a308e57960
Enderman-07 <104712238+Enderman-07@users.noreply.github.com>
2024-06-10 19:17:56 +0800

    Add files via upload

--- change_img.js
+++ change_img.js
@@ -2,55 +2,29 @@
 function changeImage1() {
     var phone = document.getElementById("phones").value; // 获取下拉列表的值  
     var image = document.getElementById("PhoneImage"); // 获取图片元素  
+    // 根据不同的值设置不同的图片  
     image.src = "./photo/" + phone + ".jpg";
-    // 根据不同的值设置不同的图片URL  
+    
 }
 
-//优点词云
+//词云
 function changeImage2() {
     var phone = document.getElementById("phones").value; // 获取下拉列表的值  
     var image = document.getElementById("text"); // 获取图片元素  
-
-    // 根据不同的值设置不同的图片URL  
-    switch (phone) {
-        case "volvo":
-            image.src = "./temp2.png";
-            break;
-        case "saab":
-            image.src = "./temp3.jpg";
-            break;
-        case "mercedes":
-            image.src = "./temp2.png";
-            break;
-        case "audi":
-            image.src = "./temp3.jpg";
-            break;
-        default:
-            image.src = "./temp2.jpg"; // 默认图片或者空图片  
-    }
+    image.src = "./word_cloud/" + phone + "/" + phone + "_keywords.jpg";
 }
-//缺点词云
+//情绪统计
 function changeImage3() {
     var phone = document.getElementById("phones").value; // 获取下拉列表的值  
     var image = document.getElementById("text"); // 获取图片元素  
+    image.src = "./word_cloud/" + phone + "/" + phone + "_emotion_analysis.jpg";
+}
 
-    // 根据不同的值设置不同的图片URL  
-    switch (phone) {
-        case "volvo":
-            image.src = "./temp2.png";
-            break;
-        case "saab":
-            image.src = "./temp3.jpg";
-            break;
-        case "mercedes":
-            image.src = "./temp2.png";
-            break;
-        case "audi":
-            image.src = "./temp3.jpg";
-            break;
-        default:
-            image.src = "./temp2.jpg"; // 默认图片或者空图片  
-    }
+//好评差评统计
+function changeImage4() {
+    var phone = document.getElementById("phones").value; // 获取下拉列表的值  
+    var image = document.getElementById("text"); // 获取图片元素  
+    image.src = "./word_cloud/" + phone + "/" + phone + "_feedback.jpg";
 }
 
 
--- read_json.js
+++ read_json.js
@@ -14,31 +14,56 @@ function loadJSON(callback) {
 function displayData(data) {
     var jsonData = JSON.parse(data);
     var container1 = document.getElementById('parameter1');
-    container1.innerHTML = '内存:' + jsonData[0]['内存'] + '<br>CPU型号:' + jsonData[0]['CPU型号'];
     var phone = getURL()
     for (let i = 0; i < jsonData.length; ++i) {
         if (jsonData[i]["机型"] == phone) {
-            container1.innerHTML = '内存:' + jsonData[i]['内存'] + '<br>CPU型号:' + jsonData[i]['CPU型号'];
+            container1.innerHTML = '品牌' + jsonData[i]['品牌']
+                + '<br>CPU型号:' + jsonData[i]['CPU型号']
+                + '<br>内存:' + jsonData[i]['内存']
+                + '<br>机身颜色:' + jsonData[i]['机身颜色']
+                + '<br>是否支持IPv6:' + jsonData[i]['支持IPv6']
+                + '<br>屏幕材质:' + jsonData[i]['屏幕材质']
+                + '<br>特质:' + jsonData[i]['特征特质'] + ';' + jsonData[i]['网络支持.5G网络']
+                + '<br>屏幕分辨率:' + jsonData[i]['屏幕分辨率']
+                + '<br>基本信息:' + jsonData[i]['基本信息.机身尺寸'] + ';' + jsonData[i]['基本信息.机身重量']
+                + '<br>屏幕:' + jsonData[i]['屏幕.屏幕尺寸'] + ';' + jsonData[i]['屏幕.屏幕刷新率']
+                + '<br>电池:' + jsonData[i]['电池信息.充电功率'] + ';' + jsonData[i]['电池信息.电池容量']
+                + '<br>操作系统:' + jsonData[i]['操作系统.系统']
+                + '<br>网络:' + jsonData[i]['网络支持.5G网络'] + ';' + jsonData[i]['网络支持.SIM卡类型'] + ';' + jsonData[i]['网络支持.双卡机类型']
+                + '<br>摄像头: 前摄' + jsonData[i]['前摄主像素'] + ';后摄' + jsonData[i]['后摄主像素'] + '/' + jsonData[i]['后摄2-超广角像素'] + '/' + jsonData[i]['后摄3-tele像素'];
             break;
         }
     }
     var container2 = document.getElementById("parameter2");
-    container2.innerHTML = '内存:' + jsonData[0]['内存'] + '<br>CPU型号:' + jsonData[0]['CPU型号'];
     var phone2 = document.getElementById("phones").value;
     for (let i = 0; i < jsonData.length; ++i) {
         if (jsonData[i]["机型"] == phone2) {
-            container2.innerHTML = '内存:' + jsonData[i]['内存'] + '<br>CPU型号:' + jsonData[i]['CPU型号'];
+            container2.innerHTML = '品牌' + jsonData[i]['品牌']
+            + '<br>CPU型号:' + jsonData[i]['CPU型号']
+            + '<br>内存:' + jsonData[i]['内存']
+            + '<br>机身颜色:' + jsonData[i]['机身颜色']
+            + '<br>是否支持IPv6:' + jsonData[i]['支持IPv6']
+            + '<br>屏幕材质:' + jsonData[i]['屏幕材质']
+            + '<br>特质:' + jsonData[i]['特征特质'] + ';' + jsonData[i]['网络支持.5G网络']
+            + '<br>屏幕分辨率:' + jsonData[i]['屏幕分辨率']
+            + '<br>基本信息:' + jsonData[i]['基本信息.机身尺寸'] + ';' + jsonData[i]['基本信息.机身重量']
+            + '<br>屏幕:' + jsonData[i]['屏幕.屏幕尺寸'] + ';' + jsonData[i]['屏幕.屏幕刷新率']
+            + '<br>电池:' + jsonData[i]['电池信息.充电功率'] + ';' + jsonData[i]['电池信息.电池容量']
+            + '<br>操作系统:' + jsonData[i]['操作系统.系统']
+            + '<br>网络:' + jsonData[i]['网络支持.5G网络'] + ';' + jsonData[i]['网络支持.SIM卡类型'] + ';' + jsonData[i]['网络支持.双卡机类型']
+            + '<br>摄像头: 前摄' + jsonData[i]['前摄主像素'] + ';后摄' + jsonData[i]['后摄主像素'] + '/' + jsonData[i]['后摄2-超广角像素'] + '/' + jsonData[i]['后摄3-tele像素'];
             break;
         }
     }
 }
 
+
 function change_opt(data) {
     var jsonData = JSON.parse(data);
-    // 清空select元素中的现有选项
+    // 清空select元素中的现有选项  
     document.getElementById('phones').innerHTML = '';
 
-    // 遍历API返回的选项列表，并添加到select元素中
+    // 遍历API返回的选项列表，并添加到select元素中  
     jsonData.forEach(option => {
         let opt = document.createElement('option');
         opt.value = option['机型'];
@@ -48,8 +73,10 @@ function change_opt(data) {
 }
 
 
+
+
 function goToPage2(event) {
-    event.preventDefault(); // 阻止默认的链接跳转行为
+    event.preventDefault(); // 阻止默认的链接跳转行为  
     var value = document.getElementById("phones").value;
     var url = 'index2.html?value=' + encodeURIComponent(value);
     window.open(url, '_blank'); // 在新标签页中打开URL  
@@ -58,6 +85,9 @@ function goToPage2(event) {
 
 function getURL() {
     var urlParams = new URLSearchParams(window.location.search);
-    var v = urlParams.get('value');
-    return v;
+    var value = urlParams.get('value');
+    if (value) {
+        return value;
+    }
+    return null;
 }
